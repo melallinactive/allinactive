e78dca2e59e0acce7a00b84dc954dc1d10d455bd
Melanie Deyla <melaniedeylaaa@gmail.com>
2026-05-30 21:00:52 +1000

    fix jinja template response

--- main.py
+++ main.py
@@ -3,22 +3,32 @@ from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse(request, "home.html")
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html"
+    )
 
 
 @app.get("/explore", response_class=HTMLResponse)
 def explore(request: Request):
-    return templates.TemplateResponse("explore.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="explore.html"
+    )
 
 
 @app.get("/templates/style.css")
 def style():
-    return FileResponse("templates/style.css", media_type="text/css")
+    return FileResponse(
+        "templates/style.css",
+        media_type="text/css"
+    )
 
 
 @app.get("/favicon.ico")
